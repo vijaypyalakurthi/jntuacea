5a8e39ab94617776cecaa1b85d38be0a2fd51cdd
sirivella madhu <madhuurs2@gmail.com>
2016-10-05 21:27:50 +0530

    Create hello_world.cpp

---
+++ MCA_SEM_1_OOPS_C/hello_world.cpp
@@ -0,0 +1,13 @@
+// ***    A simple hello world program in C++
+// ***    Student name :   sirivella madhu
+
+#include <iostream>
+using namespace std;
+
+ 
+int main()
+{
+  cout << "Hello World!";
+  
+  return 0;
+}
