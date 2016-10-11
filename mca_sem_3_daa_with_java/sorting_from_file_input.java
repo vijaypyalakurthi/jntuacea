e52773ac4a2409916f26fdcde7b084ad7fb20d4d
sirivella madhu <madhuurs2@gmail.com>
2016-10-11 18:05:20 +0530

    Update sorting_from_file_input.java

--- mca_sem_3_daa_with_java/sorting_from_file_input.java
+++ mca_sem_3_daa_with_java/sorting_from_file_input.java
@@ -3,44 +3,44 @@
 
 import java.io.*;
 import java.util.*;
-class Malli{
-	public static void main(String[] args)throws IOException
+class Sorting{
+      public static void main(String[] args)throws IOException
 	{
-	FileInputStream fs=new FileInputStream("malli.txt");
-	Scanner s=new Scanner(fs);
-	int size=fs.available();
-	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	System.out.println("enter n value:");
-	int n=Integer.parseInt(br.readLine());
-	int num=0,m=0;
-	long start =System.currentTimeMillis();
-	int a[]=new int [n];
-	while(s.hasNext() && num<n){
-	num++;
-String data=s.nextLine();
-int i=Integer.parseInt(data);
+	  FileInputStream fs=new FileInputStream("data.txt");
+	  Scanner s=new Scanner(fs);
+	  int size=fs.available();
+	  BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	  System.out.println("enter n value:");
+	  int n=Integer.parseInt(br.readLine());
+	  int num=0,m=0;
+	  long start =System.currentTimeMillis();
+	  int a[]=new int [n];
+	  while(s.hasNext() && num<n){
+	  num++;
+          String data=s.nextLine();
+	  int i=Integer.parseInt(data);
  
-if(m < n)
-{
-a[m]=i;
-m++;
-}
-}
-System.out.println("before sorting :");
-for(m=0;m<n;m++)
-System.out.println(a[m]);
-for(int x=0;x<n;x++)
-{
-for(int y=0;y<=x;y++)
-{
-if(a[x]<=a[y])
-{
-int t=a[x];
-a[x]=a[y];
-a[y]=t;
-}
-}
-}
+	  if(m < n)
+	  {
+	   a[m]=i;
+	   m++;
+	  }
+	}
+     System.out.println("before sorting :");
+     for(m=0;m<n;m++)
+     System.out.println(a[m]);
+     for(int x=0;x<n;x++)
+     {
+       for(int y=0;y<=x;y++)
+       {
+	if(a[x]<=a[y])
+        {
+	 int t=a[x];
+	 a[x]=a[y];
+	 a[y]=t;
+	}
+      }
+    }
 System.out.println("after sorting:");
 for(int k=0;k<n;k++)
 System.out.println(a[k]);
